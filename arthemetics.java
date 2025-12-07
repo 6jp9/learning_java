cf82d603fe0414afe4af5c45d3dc4a722673b4fa
6jp9 <jayaprakash.peddi619@gmail.com>
2025-12-08 00:02:34 +0530

    scanner in java

---
+++ arthemetics.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class arthemetics {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        // int a=5;
+        // int b=4;
+        // int c=0;
+        //c=a+b;
+        // c=a-b;
+        // c=a*b;
+        // c=a/b;
+        // c=a%b;
+        // or else you can directly modify one var
+        // a=a+b;
+        // or like in python
+        // a+=b;
+        // a-=b;
+        // a*=b;.....................
+        
+        // increments and decrements
+        // a++;
+        // a++;
+        // a--;
+
+        //  ORDER OF OPERATIONS [P - E - M - D - A - S](Please Excuse My Dumb Assness, Sorry!!)
+        int a = 3+4*2-1/12;
+        System.out.println(a);
+        scan.close();
+
+    }
+}
---
+++ scanner.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class scanner {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        /*
+            Scanner know how to read input but don't know where to read it. 
+            so System.in (carries the input stream and)provide the raw keyboard input from user.
+            ---Bro can eat. but don't know what to eat. "System.in" is serving bro what to eat.---
+        */
+        // System.out.print("enter name: ");
+        // String name = scn.nextLine();
+        // System.out.print("enter age: ");
+        // int age = scn.nextInt();
+        // System.out.print("enter CGPA: ");
+        // double gps= scn.nextDouble();
+        // System.out.print("Are you a student (true/false): ");
+        // Boolean isStudent = scn.nextBoolean();
+        // /* 
+        // next() --> reads the immediate word ex: input= hi hello --> cosiders input=hi
+        // nextLine() --> reads the immediate line. will consider anything until we hit enter.
+        //  */
+        // System.out.println("Name: "+name);
+        // System.out.println("age: "+age);
+        // System.out.println("Is a student: "+isStudent);
+        // System.out.println("cgpa:"+gps);
+
+
+        // some issues with scanner
+        System.out.print("enter a number: ");
+        int num=scn.nextInt();
+        scn.nextLine(); // to avoid the issue mentioned below.....v..........
+        System.out.print("enter a string: ");
+        String string = scn.nextLine();
+
+        System.out.println("num: "+num);
+        System.out.println("string: "+string);
+        /*
+        output:
+`        enter a number: 23
+ `       enter a string: num: 23
+  `      string:
+
+        -- nextInt() reads only the number, but NOT the newline (\n) you pressed after typing it.
+            so when we enter an int ex:23 and hit enter. the System.in serves it as "2 3 \n".
+            but the nextInt() only reads '23'. and the "\n" still remains in the input buffer.
+        -- nextLine() method reads the nextline ("\n" from previous input) from input buffer. and consumes it...
+        -- to avoid this we use a scn.nextLine() after the nextInt() done. so this new nextLine() will clear the input buffer.
+            since we are not assigning it to any variable. no memory is being wasted and not stored at all.
+        */
+
+        scn.close();
+        // must close the scanner after being used. might causes problems. will see in the future...
+    }
+}
