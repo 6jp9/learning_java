54ac21c0a3d1b0a526707bd8d2049e976749baed
6jp9 <jayaprakash.peddi619@gmail.com>
2025-12-07 22:29:25 +0530

    java basics

---
+++ hello.java
@@ -0,0 +1,11 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("ln for second line like \\n in python we can use them here as well");
+        // sout for --> System.out.println();
+        // we use escape character / here <-- a single comment
+        /*for multi-line comment 
+         we use "/""*" -"*""/" */
+
+    }
+}
---
+++ variables.java
@@ -0,0 +1,36 @@
+public class variables {
+    
+    /*
+        variables -- container that holds the value
+        
+        primitive variables - simple value directly stored in memory (Stack)
+        reference variables - stores address in memory (Stack) that points to something complex (Heap)
+    
+        datatypes
+            primitive datatypes:
+                int -- 9,10,888...........
+                double -- 2.333, 32.22, 99.99.........(float values)
+                char -- 'M','F','A','B','O'.......one single character -- use '' single quots
+                boolean -- true, false
+                .........soon. will see it in future or later
+            
+            reference datatypes:
+                String -- "hello", "123abc!=----023094".........u know this...must use "" double quots
+                and then there is arrays and objects will see that later
+            
+    
+    */
+   public static void main(String[] args) {
+        int age = 23;
+        double height = 5.6;
+        char gender = 'M';
+        boolean isIndian = true;
+        String name = "Jaya Prakash";
+        String place = "Vijayawada";
+        System.out.println("Hi, this is " + name + " from " + place);
+        System.out.println("I am " + age + " years out.\nAnd i am " + height + " feet tall.");
+        System.out.println("gender: " + gender +"\nIs Indian: " + isIndian);
+
+   }
+    
+}
