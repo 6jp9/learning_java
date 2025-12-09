6a2dff21360a2439e5c5e1bad3db90310619d71c
6jp9 <jayaprakash.peddi619@gmail.com>
2025-12-09 23:57:02 +0530

    logocal operators and loops

---
+++ P14_LogicalOperators_and_Loops.java
@@ -0,0 +1,26 @@
+public class P14_LogicalOperators_and_Loops {
+    public static void main(String[] args) {
+        
+        // && --> And
+        // || --> Or
+        // !  --> Not
+        // ####################
+
+        // While loops
+        // syntax : while(condition){  block  };
+
+        int a=1;
+        while (a<=10) {
+            System.out.println(a);
+            a++;
+        }
+
+        // for loop
+        // syntax: for(counter; condition; step(or increment)){    block   };
+
+        for(int i=1; i<=10 ; i++){
+            System.out.println(i);
+        };
+
+    }
+}
